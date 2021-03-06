1779feb9daa1ebf2f229b9e8bda9a2d86fb39a7f
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2021-03-06 09:07:41 +0100

    product-list .0.9.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -16,15 +16,34 @@ const getProducts = () =>{
 const showProducts=(products) =>{
   const resultArea = document.querySelector('.products-list')
   products.forEach((product)=>{
-      const item = document.createElement('div');
-      item.className = 'product';
-      item.innerHTML = `
-      <img class = "product__image src="${product.picture}">
-      <div class = "product__name">${product.name}</div>
-      <div class = "product__size">${product.size}</div>
-      <div class = "product__price">${product.price}</div>
-      `
-      resultArea.appendChild(item);
+      if(product.oldPrice===undefined){
+        const item = document.createElement('div');
+        item.className = 'product';
+        item.innerHTML = `
+        <button class = "product__like-button"><i class="fab fa-gratipay"></i></button>
+        <img class = "product__image src="${product.picture}">
+        <div class = "product__name">${product.name}</div>
+        <div class = "product__size">${product.size}</div>
+        <div class = "price__wrapper">
+        <div class = "product__price">${product.price}</div>
+        </div>        
+        `
+        resultArea.appendChild(item);
+      }else{
+        const item = document.createElement('div');
+        item.className = 'product';
+        item.innerHTML = `
+        <button class = "product__like-button"><i class="fab fa-gratipay"></i></button>
+        <img class = "product__image src="${product.picture}">
+        <div class = "product__name">${product.name}</div>
+        <div class = "product__size">${product.size}</div>
+        <div class = "price__wrapper">
+        <div class = "product__price">${product.price}</div>
+        <div class = "old__price">${product.oldPrice}</div>
+        </div>        
+        `
+        resultArea.appendChild(item);
+      }
   })
 }
 
